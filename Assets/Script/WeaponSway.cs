9895320c38d86284b7558f016f43160e3b2cf5a3
MitulChhipa <mitulchhipa5735@gmail.com>
2023-09-18 18:27:10 +0530

    v4

--- Assets/Script/WeaponSway.cs
+++ Assets/Script/WeaponSway.cs
@@ -7,6 +7,8 @@ public class WeaponSway : MonoBehaviour
     [SerializeField] private float _smooth;
     [SerializeField] private float _swayMultiplier;
     [SerializeField] private CharacterController _characterController;
+    [SerializeField] private Vector2 _clampingMax;
+    [SerializeField] private Vector2 _clampingMin;
     private Vector3 _rotationVector;
     private Quaternion _rotation;
     
@@ -15,6 +17,9 @@ public class WeaponSway : MonoBehaviour
     {
         _rotationVector.y = Input.GetAxis("Mouse X") * _swayMultiplier;
         _rotationVector.x = -Input.GetAxis("Mouse Y") * _swayMultiplier + _characterController.velocity.y*2f;
+
+        _rotationVector.x = Mathf.Clamp(_rotationVector.x, _clampingMin.x, _clampingMax.x);
+        _rotationVector.y = Mathf.Clamp(_rotationVector.y, _clampingMin.y, _clampingMax.y);
 
         _rotation = Quaternion.Euler(_rotationVector);
         transform.localRotation = Quaternion.Slerp(transform.localRotation, _rotation, _smooth*Time.deltaTime);
